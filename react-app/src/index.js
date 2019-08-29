ecccb7426e9a0ed0c735f14a580c9956212da6f3
wangzhongjie <uvdream@163.com>
2019-08-29 14:06:05 +0800

    [feat] antd

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.less';
+import 'antd/dist/antd.css';
 import App from './pages/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
