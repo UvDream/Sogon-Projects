19d255836a870ca00d18ab7aa83204a9c8a51928
wangzhongjie <UvDream@163.com>
2019-09-17 13:40:05 +0800

    [feat] :tada:引入iconfont

--- data-monitor/src/App.tsx
+++ data-monitor/src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.less';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>文字</span>
+      <span className="iconfont icon-gerenzhongxinxuanzhong1"></span>
     </div>
   );
 }
