db1d6186056e9b735fce99685702181bc7125af1
wangzhongjie <UvDream@163.com>
2019-09-17 16:09:19 +0800

    [feat] :tada: 菜单动态

--- data-monitor/src/pages/index.tsx
+++ data-monitor/src/pages/index.tsx
@@ -1,60 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import DashBoard from "./dashboard/index"
 import OperationLog from "./operation-log/index"
 import { Layout, Menu, Icon } from 'antd';
 import "../styles/index.less"
 const { Header, Sider, Content } = Layout;
-const Home: React.FC = () => {
-    let state = {
+export default class Home extends Component {
+    state = {
         collapsed: false,
     };
-
-    return (
-        <div className="main">
-            <Layout>
-                <Sider trigger={null} collapsible collapsed={state.collapsed}>
-                    <div className="logo" />
-                    <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                        <Menu.Item key="1">
-                            <Icon type="user" />
-                            <span>nav 1</span>
-                        </Menu.Item>
-                        <Menu.Item key="2">
-                            <Icon type="video-camera" />
-                            <span>nav 2</span>
-                        </Menu.Item>
-                        <Menu.Item key="3">
-                            <Icon type="upload" />
-                            <span>nav 3</span>
-                        </Menu.Item>
-                    </Menu>
-                </Sider>
+    render() {
+        return (
+            <div className="main" >
                 <Layout>
-                    <Header style={{ background: '#fff', padding: 0 }}>
-                        <Icon
-                            className="trigger"
-                            type={state.collapsed ? 'menu-unfold' : 'menu-fold'}
-                        />
-                    </Header>
-                    <Content
-                        style={{
-                            margin: '24px 16px',
-                            padding: 24,
-                            background: '#fff',
-                            minHeight: 280,
-                        }}
-                    >
-                        <Route path="/" exact component={DashBoard}></Route>
-                        <Route path="/operationLog/" component={OperationLog}></Route>
-                    </Content>
+                    <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                        <div className="logo" />
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">
+                                <Icon type="user" />
+                                <span>nav 1</span>
+                            </Menu.Item>
+                            <Menu.Item key="2">
+                                <Icon type="video-camera" />
+                                <span>nav 2</span>
+                            </Menu.Item>
+                            <Menu.Item key="3">
+                                <Icon type="upload" />
+                                <span>nav 3</span>
+                            </Menu.Item>
+                        </Menu>
+                    </Sider>
+                    <Layout>
+                        <Header style={{ background: '#fff', padding: 0 }}>
+                            <Icon
+                                className="trigger"
+                                type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+                                onClick={this.toggle}
+                            />
+                        </Header>
+                        <Content
+                            style={{
+                                margin: '24px 16px',
+                                padding: 24,
+                                background: '#fff',
+                                minHeight: 280,
+                            }}
+                        >
+                            <Route path="/" exact component={DashBoard}></Route>
+                            <Route path="/operationLog/" component={OperationLog}></Route>
+                        </Content>
+                    </Layout>
                 </Layout>
-            </Layout>
-            {/* 这才是首页
+                {/* 这才是首页
             <Link to="/operationLog/">仪表盘</Link>
             <Route path="/" exact component={DashBoard}></Route>
             <Route path="/operationLog/" component={OperationLog}></Route> */}
-        </div>
-    );
+            </div>
+        );
+    }
+
+    toggle = () => {
+        this.setState({
+            collapsed: !this.state.collapsed,
+        });
+    }
 }
-export default Home;
