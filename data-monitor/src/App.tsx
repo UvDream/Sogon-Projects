cf01b04680225ec726b3e383b238ef377b913441
wangzhongjie <UvDream@163.com>
2019-09-17 13:56:51 +0800

    [feat] :tada: antdesign

--- data-monitor/src/App.tsx
+++ data-monitor/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.less';
+import Button from 'antd/es/button';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <span>文字</span>
       <span className="iconfont icon-gerenzhongxinxuanzhong1"></span>
+      <Button type="primary">Button</Button>
     </div>
   );
 }
