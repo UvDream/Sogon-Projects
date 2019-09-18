e3c2efe92ad2681a4d8bf8a5d2064a105dfd7da3
wangzhongjie <UvDream@163.com>
2019-09-18 14:15:00 +0800

    [feat] :tada: 仪表盘

--- data-monitor/src/components/menus/index.tsx
+++ data-monitor/src/components/menus/index.tsx
@@ -2,22 +2,34 @@
  * @Author: wangzhongjie
  * @Date: 2019-09-17 17:06:35
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2019-09-17 17:17:57
+ * @LastEditTime: 2019-09-18 14:14:24
  * @Description: 菜单
  * @Email: UvDream@163.com
  */
 import React, { Component, Fragment } from 'react';
 import { Menu, Icon } from 'antd';
+import { Link } from "react-router-dom";
+
+import { ClickParam } from 'antd/es/menu';
+
+
 const { SubMenu } = Menu;
 
+
 export default class Menus extends Component {
+    menusCLick = (event: ClickParam) => {
+        console.log("菜单点击", event)
+    }
     render() {
         return (
             <Fragment>
-                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+                <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} onClick={this.menusCLick}>
                     <Menu.Item key="1">
-                        <Icon type="dashboard" />
-                        <span>仪表盘</span>
+                        <Link to="/">
+                            <Icon type="dashboard" />
+                            <span>仪表盘</span>
+                        </Link>
+
                     </Menu.Item>
                     <SubMenu
                         key="2"
@@ -28,12 +40,18 @@ export default class Menus extends Component {
                             </span>
                         }
                     >
-                        <Menu.Item key="5">操作日志</Menu.Item>
+                        <Menu.Item key="5">
+                            <Link to="/operationLog">
+                                操作日志
+                            </Link>
+                        </Menu.Item>
+
                         <Menu.Item key="6">运行日志</Menu.Item>
                     </SubMenu>
 
                 </Menu>
-            </Fragment>
+            </Fragment >
         )
     }
+
 }
