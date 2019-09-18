3e052609b2c735147674a6492f5bbd2abfacc058
wangzhongjie <UvDream@163.com>
2019-09-18 16:59:10 +0800

    [feat] :tada: react-redux 浏览器调试

--- data-monitor/src/store/index.ts
+++ data-monitor/src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore } from "redux";
 import reducer from "./reducers/index";
-const store = createStore(reducer);
+declare let window: any
+const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
