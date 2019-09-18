caae82603456786ed90c0ef83032fda1d86ade86
wangzhongjie <UvDream@163.com>
2019-09-18 17:01:30 +0800

    [feat] :tada: ts文档注释

--- data-monitor/src/store/index.ts
+++ data-monitor/src/store/index.ts
@@ -1,5 +1,6 @@
 import { createStore } from "redux";
 import reducer from "./reducers/index";
+// 解决ts里面问题
 declare let window: any
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 export default store;
