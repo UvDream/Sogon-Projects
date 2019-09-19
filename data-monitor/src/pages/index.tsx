7eb060c4c551a9a0f164276900f9f404be31d4ae
wangzhongjie <UvDream@163.com>
2019-09-19 11:05:15 +0800

    [feat] :tada:面包屑

--- data-monitor/src/pages/index.tsx
+++ data-monitor/src/pages/index.tsx
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2019-09-17 16:40:41
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2019-09-17 17:15:30
+ * @LastEditTime: 2019-09-19 10:57:00
  * @Description: 主界面
  * @Email: UvDream@163.com
  */
@@ -10,7 +10,7 @@ import React, { Component } from 'react';
 import { Route } from "react-router-dom";
 import DashBoard from "./dashboard/index"
 import OperationLog from "./operation-log/index"
-import { Layout, Icon } from 'antd';
+import { Layout, Icon, Breadcrumb, Avatar } from 'antd';
 import "../styles/index.less"
 import Menus from "../components/menus/index"
 const { Header, Sider, Content } = Layout;
@@ -27,16 +27,20 @@ export default class Home extends Component {
                         <Menus />
                     </Sider>
                     <Layout>
-                        <Header style={{ background: '#fff', width: "100%", position: 'fixed', padding: 0 }}>
+                        <Header style={{ background: '#fff', width: "100%", position: 'fixed', padding: 0, display: "flex", alignItems: "center" }}>
                             <Icon
                                 className="trigger"
                                 type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
                                 onClick={this.toggle}
                             />
-                            {/* <Breadcrumb style={{ position: "fixed" }}>
-                                <Breadcrumb.Item>User</Breadcrumb.Item>
-                                <Breadcrumb.Item>Bill</Breadcrumb.Item>
-                            </Breadcrumb> */}
+                            <Breadcrumb style={{ marginLeft: "20px" }}>
+                                <Breadcrumb.Item>首页</Breadcrumb.Item>
+                                <Breadcrumb.Item>仪表盘</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div style={{ position: "fixed", right: "20px", display: "flex", alignItems: "center" }}>
+                                <Avatar size="large" icon="user" />
+                                <div style={{ marginLeft: "10px" }}>root</div>
+                            </div>
                         </Header>
                         <Content
                             style={{
