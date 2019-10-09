6e2feb8e9323d62c1fde8a9593a5004c4a6f51a0
wangzhongjie <UvDream@163.com>
2019-10-09 10:43:19 +0800

    [feat] icon

--- kunshan/kunshan-admin/src/components/menus/menus.vue
+++ kunshan/kunshan-admin/src/components/menus/menus.vue
@@ -7,7 +7,7 @@
     :style="{ height: '100%', borderRight: 0 }"
   >
     <a-menu-item v-for="(item,index) in menusList" :key="index">
-      <a-icon type="mail" />
+      <a-icon :type="item.icon" />
       {{item.name}}
     </a-menu-item>
 
@@ -29,51 +29,63 @@ export default {
       menusList: [
         {
           id: 1,
-          name: "值班情况"
+          name: "值班情况",
+          icon: "folder-open"
         },
         {
           id: 2,
-          name: "警情质态"
+          name: "警情质态",
+          icon: "folder-open"
         },
         {
           id: 3,
-          name: "科技技防运行质态"
+          name: "科技技防运行质态",
+          icon: "folder-open"
         },
         {
           id: 4,
-          name: "平台预警"
+          name: "平台预警",
+          icon: "folder-open"
         },
         {
           id: 5,
-          name: "巡逻盘查质态"
+          name: "巡逻盘查质态",
+          icon: "folder-open"
         },
         {
           id: 6,
-          name: "现场勘查质态"
+          name: "现场勘查质态",
+          icon: "folder-open"
         },
         {
           id: 7,
-          name: "扫黑专项斗争情况"
+          name: "扫黑专项斗争情况",
+          icon: "folder-open"
         },
         {
           id: 8,
-          name: "一案一档一研判"
+          name: "一案一档一研判",
+          icon: "folder-open"
         },
         {
           id: 9,
-          name: "笔录制作质态"
+          name: "笔录制作质态",
+          icon: "folder-open"
         },
         {
           id: 10,
-          name: "入所人员管理质态"
+          name: "入所人员管理质态",
+          icon: "folder-open"
         },
         {
           id: 11,
-          name: "研判分析"
+          name: "研判分析",
+          icon: "folder-open"
         },
         {
           id: 12,
-          name: "其他"
+          name: "其他",
+          icon: "folder-open"
         }
       ]
     };
