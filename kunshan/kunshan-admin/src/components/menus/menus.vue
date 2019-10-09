2e525e6f0ad70d9c4ba14bf1c39506be3dad156f
wangzhongjie <UvDream@163.com>
2019-10-09 10:36:33 +0800

    [feat] logo

---
+++ kunshan/kunshan-admin/src/components/menus/menus.vue
@@ -0,0 +1,84 @@
+<template>
+  <a-menu
+    mode="inline"
+    :defaultSelectedKeys="['1']"
+    theme="dark"
+    :defaultOpenKeys="['sub1']"
+    :style="{ height: '100%', borderRight: 0 }"
+  >
+    <a-menu-item v-for="(item,index) in menusList" :key="index">
+      <a-icon type="mail" />
+      {{item.name}}
+    </a-menu-item>
+
+    <!-- <a-sub-menu key="sub1">
+        <span slot="title">
+          <a-icon type="user" />subnav 1
+        </span>
+        <a-menu-item key="1">option1</a-menu-item>
+        <a-menu-item key="2">option2</a-menu-item>
+        <a-menu-item key="3">option3</a-menu-item>
+        <a-menu-item key="4">option4</a-menu-item>
+    </a-sub-menu>-->
+  </a-menu>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      menusList: [
+        {
+          id: 1,
+          name: "值班情况"
+        },
+        {
+          id: 2,
+          name: "警情质态"
+        },
+        {
+          id: 3,
+          name: "科技技防运行质态"
+        },
+        {
+          id: 4,
+          name: "平台预警"
+        },
+        {
+          id: 5,
+          name: "巡逻盘查质态"
+        },
+        {
+          id: 6,
+          name: "现场勘查质态"
+        },
+        {
+          id: 7,
+          name: "扫黑专项斗争情况"
+        },
+        {
+          id: 8,
+          name: "一案一档一研判"
+        },
+        {
+          id: 9,
+          name: "笔录制作质态"
+        },
+        {
+          id: 10,
+          name: "入所人员管理质态"
+        },
+        {
+          id: 11,
+          name: "研判分析"
+        },
+        {
+          id: 12,
+          name: "其他"
+        }
+      ]
+    };
+  }
+};
+</script>
+<style lang="less" scoped>
+</style>
--- kunshan/kunshan-admin/src/views/index.vue
+++ kunshan/kunshan-admin/src/views/index.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    呵呵xxx
-    <router-view></router-view>
-  </div>
+  <a-layout id="components-layout-demo-top-side-2" style="height:100vh">
+    <a-layout-header class="header">
+      <Logo></Logo>
+    </a-layout-header>
+    <a-layout>
+      <a-layout-sider width="200" style="background: #fff">
+        <Menus></Menus>
+      </a-layout-sider>
+      <a-layout>
+        <!-- <a-breadcrumb style="margin: 16px 0">
+          <a-breadcrumb-item>Home</a-breadcrumb-item>
+          <a-breadcrumb-item>List</a-breadcrumb-item>
+          <a-breadcrumb-item>App</a-breadcrumb-item>
+        </a-breadcrumb>-->
+        <a-layout-content
+          :style="{ background: '#fff', padding: '10px', margin: 0, minHeight: '280px' }"
+        >
+          <router-view></router-view>
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-export default {};
+import Menus from "../components/menus/menus";
+import Logo from "../components/logo/logo";
+export default {
+  data() {
+    return {
+      collapsed: false
+    };
+  },
+  components: {
+    Menus,
+    Logo
+  }
+};
 </script>
 
-<style>
+<style lang="less" scoped>
+.header {
+  padding-left: 10px;
+}
 </style>
