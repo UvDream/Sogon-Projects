46bd796813b043706617d6a5f766c5befb10c2d5
wangzhongjie <UvDream@163.com>
2019-10-09 16:04:52 +0800

    [feat] 路由跳转

--- kunshan/kunshan-admin/src/components/menus/menus.vue
+++ kunshan/kunshan-admin/src/components/menus/menus.vue
@@ -5,21 +5,12 @@
     theme="dark"
     :defaultOpenKeys="['sub1']"
     :style="{ height: '100%', borderRight: 0 }"
+    @click="menusClick"
   >
     <a-menu-item v-for="(item,index) in menusList" :key="index">
       <a-icon :type="item.icon" />
       {{item.name}}
     </a-menu-item>
-
-    <!-- <a-sub-menu key="sub1">
-        <span slot="title">
-          <a-icon type="user" />subnav 1
-        </span>
-        <a-menu-item key="1">option1</a-menu-item>
-        <a-menu-item key="2">option2</a-menu-item>
-        <a-menu-item key="3">option3</a-menu-item>
-        <a-menu-item key="4">option4</a-menu-item>
-    </a-sub-menu>-->
   </a-menu>
 </template>
 <script>
@@ -87,8 +78,27 @@ export default {
           name: "其他",
           icon: "folder-open"
         }
+      ],
+      routes: [
+        "/dashboard",
+        "/policeQuality",
+        "/technicalDefense",
+        "/platformWarning",
+        "/patrolCheck",
+        "/siteSurvey",
+        "/blackSweep",
+        "/caseStudy",
+        "/transcriptProduction",
+        "/personnelManagement",
+        "/analysis",
+        "/other"
       ]
     };
+  },
+  methods: {
+    menusClick(e) {
+      this.$router.push(this.routes[e.key]);
+    }
   }
 };
 </script>
