9d741e1a2c0b08d00a1e3ad9a7239606b863d386
wangzhongjie <UvDream@163.com>
2019-10-22 11:46:43 +0800

    [feat] :tada: 初始化

---
+++ psychiatric-management-admin/src/views/login/login.vue
@@ -0,0 +1,18 @@
+<!--
+ * @Author: wangzhongjie
+ * @Date: 2019-10-22 11:35:47
+ * @LastEditors: wangzhongjie
+ * @LastEditTime: 2019-10-22 11:36:03
+ * @Description: 登陆注册界面
+ * @Email: UvDream@163.com
+ -->
+<template>
+  <div>login</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
