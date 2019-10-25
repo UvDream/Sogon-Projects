5f901b09f22602996190cd62688f0304eef6bb3d
xiahongxiu <790648858@qq.com>
2019-10-25 16:31:21 +0800

    登录

--- psychiatric-management-admin/src/views/login/login.vue
+++ psychiatric-management-admin/src/views/login/login.vue
@@ -8,33 +8,42 @@
  -->
 <template>
   <div class="login">
+    <div class="logo">
+      <img src="../../assets/fonts/login/logo.png" />
+      <div>
+        <div>徐州重度精神病患者</div>
+        <div>帮扶管控系统</div>
+        <div>HELP MANAGEMENT AND CONTROL SYSTEM</div>
+      </div>
+    </div>
     <div class="login-block">
       <Form ref="formInline" :model="formInline" :rules="ruleInline" inline>
         <div>
-          <FormItem prop="user">
-            <Input type="text" v-model="formInline.user" placeholder="账户">
-              <Icon type="ios-person-outline" slot="prepend"></Icon>
-            </Input>
+          <h1>用户登录</h1>
+          <h2>LOGIN</h2>
+        </div>
+        <div>
+          <FormItem prop="user" class="form-block">
+            <Input prefix="ios-person-outline" placeholder="账户" type="text" v-model="formInline.user" style="width: 380px;" />
           </FormItem>
         </div>
         <div>
           <FormItem prop="password">
-            <Input
-              type="password"
-              v-model="formInline.password"
-              placeholder="密码"
-            >
-              <Icon type="ios-lock-outline" slot="prepend"></Icon>
-            </Input>
+            <Input prefix="ios-lock-outline" placeholder="密码" type="text" v-model="formInline.password" style="width: 380px;" />
           </FormItem>
         </div>
-
-        <FormItem>
-          <Button type="primary" @click="handleSubmit('formInline')"
-            >Signin</Button
-          >
-        </FormItem>
+        <div>
+          <FormItem>
+            <Checkbox v-model="single">记住密码</Checkbox>
+          </FormItem>
+        </div>
+        <div>
+          <FormItem>
+            <Button type="primary" @click="handleSubmit('formInline')" style="width: 300px;" >登录</Button>
+          </FormItem>
+        </div>
       </Form>
+      <img src="../../assets/fonts/login/bg-right.png">
     </div>
   </div>
 </template>
@@ -85,13 +94,126 @@ export default {
 };
 </script>
 
+
 <style scoped lang="less">
+body{
+  position:relative;
+  height:100%;
+  width:100%;
+  background: url('../../assets/fonts/login/bg.png') no-repeat;
+  background-size: cover;
+  overflow: hidden;  
+}
 .login {
-  &-block {
-    width: 500px;
-    height: 300px;
-    outline: 1px solid red;
-    text-align: center;
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  .logo{
+    height: 76px;
+    width: 800px;
+    left: 120px;
+    top: 120px;
+    position: absolute;
+    display: flex;
+    align-items: self-start;
+    & > img{
+      margin-right: 30px;
+    }
+    & > div{
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+      width: 605px;
+      flex-flow: wrap;
+      & > div:nth-child(1) {
+        font-size: 40px;
+        color: #333;
+        letter-spacing: .33px;
+      }
+      & > div:nth-child(2) {
+        font-size: 40px;
+        color: #53a7a9;
+        letter-spacing: .33px;
+        line-height: 56px;
+      }
+      & > div:nth-child(3) {
+        font-size: 14px;
+        color: #999999;
+        letter-spacing: 0;
+      }
+    }
+    
   }
+  &-block {
+    position: absolute;
+    top: 290px;
+    width: 990px;
+    height: 460px;
+    box-shadow: 0 0 20px 0 rgba(14,37,38,0.15);
+    display: flex;
+    justify-content: space-between;
+    border-radius: 20px;
+    & > form{
+      width: 630px;
+      height: 460px;
+      display: flex;
+      justify-content: center;
+      align-items: flex-start;
+      flex-direction: column;
+      padding-left:80px;
+      & > div{
+        & >h1{
+          font-size: 28px;
+          color: #333333;
+          letter-spacing: 3.03px;
+          font-weight:normal;
+        }
+        & >h2 {
+          font-size: 16px;
+          color: #BBBBBB;
+          font-weight:normal;
+        }
+        margin-bottom:10px;
+      }
+      & > div:nth-child(1){
+        margin-bottom:44px;
+      }
+      & > div:last-child{
+        margin-left:36px;
+      }
+      .form-block{
+        height:42px;
+      }
+    }
+    & >img{
+      background: url('../../assets/fonts/login/bg-right.png') no-repeat right;
+      background-size: cover;
+      position: absolute;
+      right: -30px;
+      top: -40px;
+    }
+    
+  }
+}
+</style>
+<style type="text/css">
+.ivu-input-wrapper .ivu-input{
+  height: 42px;
+  border-radius: 20px;
+  color: #999;
+  font-size: 16px;
+  background: #DDE2E2;
+  border: 1px solid #DDE2E2;
+}
+.ivu-input-wrapper .ivu-input-prefix, .ivu-input-wrapper .ivu-input-suffix{
+  height: 42px;
+  top:4px;
+}
+.ivu-form-item-content .ivu-btn-primary{
+  border-radius: 20px;
+  height: 40px;
 }
 </style>
